9f42efcd3709c07edbf93c4d52779ad02254a8a6
Dmytro Vadimov <104729263+DmytroVadimov@users.noreply.github.com>
2022-09-29 22:21:49 +0300

    hw_8_header-mobile

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+
+(() => {
+const refs = {
+    openMenuBtn: document.querySelector('.mobile__open'),
+closeMenuBtn: document.querySelector('.mobile__close'),
+    menu: document.querySelector('.mobile'),
+    body: document.querySelector('body'),
+};
+
+refs.openMenuBtn.addEventListener('click', toggleMenu);
+refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+
+
+function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+}
+})();
